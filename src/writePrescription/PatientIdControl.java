d89ff6ac819a9fa4dd2759306ddfb788770087be
Nabendu Bikash Binda <eng.nabendu@gmail.com>
2020-04-25 20:00:57 +0600

    develop patient id control control class for store last patient id which doctor use

--- src/writePrescription/PatientIdControl.java
+++ src/writePrescription/PatientIdControl.java
@@ -1,10 +1,47 @@
 package writePrescription;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class PatientIdControl {
-private int patientIdSrorer;
-public void LastPatientIdStore(int patientId) {
-	File doctorsLastPatientId
-}
+	private Scanner idScan;
+	private int patientIdSrorer;
+	private File doctorsLastPatientId = new File("F:/softwareDevelopment/Doctor-Assistant/PatientDatabase/idCallBack.id");
+
+//-----------this method for store id---------------in idCallback.id file--------------------
+	public void setLastPatientId(int patientId) {
+		
+		FileWriter idWriter;
+		try {
+			idWriter = new FileWriter(doctorsLastPatientId);
+			idWriter.write(patientId);
+			idWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//--------this method for read id---------from idCallBack.id file---------------------
+	public int getLastPatientId() {
+		FileReader idReader;
+		try {
+			idReader=new FileReader(doctorsLastPatientId);
+			idScan=new Scanner(idReader);
+			patientIdSrorer=idScan.nextInt();
+			try {
+				idReader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return patientIdSrorer;
+	}
+	
 }
